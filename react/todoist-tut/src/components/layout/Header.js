18c774165a32b1244ff3404e4d6825af27d115d6
Konrad Roguszewski <aker.hm@gmail.com>
2021-07-22 16:27:38 +0200

    components building setup

---
+++ react/todoist-tut/src/components/layout/Header.js
@@ -0,0 +1,17 @@
+export const Header = () => {
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>+</li>
+            <li>Pizza Slice!</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
