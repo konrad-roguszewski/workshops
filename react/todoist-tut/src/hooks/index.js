3d30de03aaa9845efa753b948a0c1e41575fe3ba
Konrad Roguszewski <aker.hm@gmail.com>
2021-07-23 15:07:20 +0200

    adding function helpers

--- react/todoist-tut/src/hooks/index.js
+++ react/todoist-tut/src/hooks/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { firebase } from '../firebase';
-
-const collatedTasksExist = () => {};
+import moment from 'moment';
+import { collatedTasksExist } from '../helpers';
 
 export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([]);
