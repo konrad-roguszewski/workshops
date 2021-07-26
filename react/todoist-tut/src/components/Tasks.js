9d3203331933f3149eeb9ea7cf59227438258c53
Konrad Roguszewski <aker.hm@gmail.com>
2021-07-26 09:02:07 +0200

    building the Checkbox component

--- react/todoist-tut/src/components/Tasks.js
+++ react/todoist-tut/src/components/Tasks.js
@@ -1,5 +1,10 @@
+import { Checkbox } from "./Checkbox";
+import { useTasks } from '../hooks';
+
 export const Tasks = () => {
-  const tasks = [];
+  const { tasks } = useTasks("1");
+
+  console.log(tasks);
 
   let projectName = '';
 
