545371d1ccce2e193de73171e47ed2f293e8c873
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-07 15:38:40 +0200

    TypeScript Forms and Events

--- react/react-typescript/src/App.tsx
+++ react/react-typescript/src/App.tsx
@@ -1,26 +1,46 @@
 import { useState } from 'react';
-import { User } from './interfaces';
+
+const defaultFormData = {
+  title: "",
+  body: "",
+};
 
 export default function App() {
-  const [user, setUser] = useState<User | null>(null);
+  const [formData, setFormData] = useState(defaultFormData);
+  const { title, body } = formData;
 
-  const fetchUser = () =>
-    setUser({
-      name: 'Mitchel',
-      age: 23,
-      country: 'the Netherlands',
-      address: {
-        street: 'Main st.',
-        number: 88,
-        zip: '21345'
-      },
-      admin: false
-    });
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+
+    setFormData(defaultFormData);
+  };
 
   return (
     <>
-      <button onClick={fetchUser}>Fetch user on click</button>
-      {user && <p>{`Welcome ${user.name}`}</p>}
+      <h1>Form</h1>
+      <p>Create a post</p>
+
+      <form onSubmit={(e) => onSubmit(e)}>
+        <label htmlFor="title">Title</label>
+        <br />
+        <input type="text" id="title" value={title} onChange={onChange}/>
+        <br />
+        <br />
+        <label htmlFor="body">Body</label>
+        <br />
+        <input type="text" id="body" value={body} onChange={onChange}/>
+        <br />
+        <br />
+        <button type="submit">Upload post</button>
+      </form>
     </>
   );
 }
