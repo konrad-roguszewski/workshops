45ac7b5cff31fe63b6b5de7d8449210256af1ea1
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-10 12:33:36 +0200

    Tuples

--- typescript/typescript-tutorial/src/app.ts
+++ typescript/typescript-tutorial/src/app.ts
@@ -17,35 +17,30 @@ const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
+
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
   
   let doc: HasFormatter;
   if (type.value === 'invoice'){
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
-  }
+    doc = new Payment(...values);
+  };
 
-  list.render(doc, type.value, 'end')
+  list.render(doc, type.value, 'end');
 });
 
-// ENUMS
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON };
-interface Resource<T> {
-  uid: number;
-  resourceType: ResourceType;
-  data: T;
-};
+// tuples
 
-const docOne: Resource<object> = {
-  uid: 1,
-  resourceType: ResourceType.BOOK,
-  data: { title: 'name of the wind' }
-};
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
-const docTwo: Resource<object> = {
-  uid: 10,
-  resourceType: ResourceType.PERSON,
-  data: { name: 'shaun' }
-};
+let tup: [string, number, boolean] = ['ryu', 25, true];
+tup[0] = 'ken';
+tup[1] = 30;
 
-console.log(docOne, docTwo)
+// let student: [string, number];
+// student = ['chun-li', 123];
