38e5188315f8fe357bf70be154667a880bc00038
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-11 11:30:21 +0200

    React components and props

---
+++ react/react-ts-course/src/components/Card.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// Styles
+import { Wrapper } from './Card.styles';
+
+type Props = {
+  name: string;
+  imgUrl: string;
+  // children: React.ReactNode;
+};
+
+const Card = ({ name, imgUrl }: Props) => (
+  <Wrapper>
+    <p>{name}</p>
+    <p>{imgUrl}</p>
+  </Wrapper>
+);
+
+// another way (diffrence in how to use types on children)
+// const Card: React.FC<Props> = ({ name, imgUrl }) => (
+//   <Wrapper>
+//     <p>{name}</p>
+//     <p>{imgUrl}</p>
+//   </Wrapper>
+// );
+
+export default Card;
