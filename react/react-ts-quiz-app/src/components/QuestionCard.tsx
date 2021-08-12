34df2c3d60d0ae2382ae9ff64824d3e4f9457c0b
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-12 13:41:40 +0200

    Changed QuestionCard jsx template to work with styles

--- react/react-ts-quiz-app/src/components/QuestionCard.tsx
+++ react/react-ts-quiz-app/src/components/QuestionCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 // Types
 import { AnswerObject } from '../App';
 
@@ -20,21 +22,25 @@ const QuestionCard: React.FC<Props> = ({
   totalQuestions
 }) => {
   return (
-    <div>
+    <Wrapper>
       <p className="number">
         Question: {questionNr} / {totalQuestions}
       </p>
       <p dangerouslySetInnerHTML={{ __html: question }} />
       <div>
         {answers.map(answer => (
-          <div key={answer}>
+          <ButtonWrapper
+            key={answer}
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+          >
             <button disabled={!!userAnswer} value={answer} onClick={callback}>
               <span dangerouslySetInnerHTML={{ __html: answer }} />
             </button>
-          </div>
+          </ButtonWrapper>
         ))}
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
