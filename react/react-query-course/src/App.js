fc9747204dd3729ebadae752499520755a2019cf
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-16 10:02:39 +0200

    Added useQuery to App

--- react/react-query-course/src/App.js
+++ react/react-query-course/src/App.js
@@ -1,9 +1,30 @@
+import { useQuery } from 'react-query';
 import './App.css';
 
+const fetchUsers = async () => {
+  const response = await fetch('https://reqres.in/api/users');
+  if (!response.ok) {
+    throw new Error('Something went wrong!');
+  }
+  return response.json();
+};
+
 function App() {
+  // Grab all users
+  const { data: users, isLoading, error } = useQuery('users', fetchUsers);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Somrthing went wrong...</p>;
+
+  console.log(users);
+
   return (
     <div className="App">
-      <p>React Query Course</p>
+      {users.data.map(user => (
+        <p key={user.id}>
+          {user.first_name} {user.last_name}
+        </p>
+      ))}
     </div>
   );
 }
