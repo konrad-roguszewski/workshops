83aeb631e3d0664086e0810ffdc19ee5a772b152
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-16 11:31:56 +0200

    Added settings to useQuery hook

--- react/react-query-course/src/App.js
+++ react/react-query-course/src/App.js
@@ -1,88 +1,37 @@
-import { useMutation, useInfiniteQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import './App.css';
 
-const fetchInfiniteUsers = async ({ pageParam = 1 }) => {
-  const response = await fetch(`https://reqres.in/api/users?page=${pageParam}`);
+const fetchUsers = async () => {
+  const response = await fetch('https://reqres.in/api/users');
   if (!response.ok) {
     throw new Error('Something went wrong!');
   }
   return response.json();
 };
 
-const addUser = async user => {
-  const response = await fetch('https://reqres.in/api/users', {
-    method: 'POST',
-    body: JSON.stringify({
-      first_name: user.first_name,
-      last_name: user.last_name
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8'
-    }
+const App = () => {
+  // Grab all users
+  const { data, isLoading, error } = useQuery('users', fetchUsers, {
+    enabled: true,
+    refetchOnWindowFocus: false,
+    refetchInterval: false
   });
 
-  if (!response.ok) {
-    throw new Error('Something went wrong!');
-  }
-
-  return response.json();
-};
-
-function App() {
-  // Call the useQueryClient hook
-  const queryClient = useQueryClient();
-
-  // Grab all users
-  const { data, isLoading, isFetching, fetchNextPage, hasNextPage, error } =
-    useInfiniteQuery('users', fetchInfiniteUsers, {
-      getNextPageParam: (lastPage, pages) => {
-        if (lastPage.page < lastPage.total_pages) return lastPage.page + 1;
-        return false;
-      }
-    });
-
-  // Create a mutation for adding a user
-  const {
-    mutate,
-    mutateAsync,
-    isLoading: isAddingUser,
-    errror: addError
-  } = useMutation(addUser);
-
-  const handleAddUser = async () => {
-    const newUser = await mutateAsync({
-      first_name: 'React Query',
-      last_name: 'Course'
-    });
-    console.log('This was an async mutation');
-    console.log(newUser);
-    // queryClient.invalidateQueries('users');
-    queryClient.setQueryData('users', oldData => ({
-      ...oldData,
-      data: [newUser, ...oldData.data]
-    }));
-  };
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error || addError) return <p>Something went wrong...</p>;
+  if (isLoading) return <p>Loading ...</p>;
+  if (error) return <p>Something went wrong ...</p>;
+  if (!data) return null;
 
   console.log(data);
 
   return (
-    <div className="App">
-      {isAddingUser ? <p>Adding user...</p> : null}
-      <button onClick={handleAddUser}>Add User</button>
-      {data.pages.map(page =>
-        page.data.map(user => (
-          <p key={user.id}>
-            {user.first_name} {user.last_name}
-          </p>
-        ))
-      )}
-      {isFetching && <p>Loading...</p>}
-      {hasNextPage && <button onClick={fetchNextPage}>Load More</button>}
+    <div className='App'>
+      {data.data.map(person => (
+        <p key={person.id}>
+          {person.first_name} {person.last_name}
+        </p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
