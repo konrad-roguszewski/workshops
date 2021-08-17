6a54800245049ebba169ecbdd4a1f096fdef0a98
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-17 16:00:03 +0200

    Project setup using cra with redux template

---
+++ react/redux-tut/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div className="App">
+      <h1> The count is: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
+    </div>
+  );
+}
+
+export default App;
