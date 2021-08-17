db00d9ec18f669728838b43b293b01aeff469330
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-17 16:32:34 +0200

    Added redux state and actions to App component

--- react/redux-tut/src/App.js
+++ react/redux-tut/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './redux/counter';
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  const { count } = useSelector(state => state.counter);
+  // const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1> The count is: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>increment</button>
-      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(3))}>
+        increment by 3
+      </button>
     </div>
   );
 }
