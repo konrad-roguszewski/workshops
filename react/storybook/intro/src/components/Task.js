02e50d07dcfd5e72eb92a00e12db1677b6d36b3e
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-18 13:13:39 +0200

    Specified data req

--- react/storybook/intro/src/components/Task.js
+++ react/storybook/intro/src/components/Task.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -13,7 +18,12 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
@@ -27,3 +37,19 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
     </div>
   );
 }
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func
+};
