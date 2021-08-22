014b717c88a749126008e0ca6dcf5d6b6dc7d8a6
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-22 12:25:16 +0200

    Mocking requests

--- react/react-test-lib/src/components/FollowersList/__test__/FollowersList.test.js
+++ react/react-test-lib/src/components/FollowersList/__test__/FollowersList.test.js
@@ -14,12 +14,13 @@ describe("FollowersList", () => {
   it('should render follower item', async () => {
     render(<MockFoloowersList />);
     const followerDivElement = await screen.findByTestId("follower-item-0");
+    screen.debug();
     expect(followerDivElement).toBeInTheDocument();
   });
 
-  it('should render multiple follower items', async () => {
-    render(<MockFoloowersList />);
-    const followerDivElements = await screen.findAllByTestId(/follower-item/i);
-    expect(followerDivElements.length).toBe(5);
-  });
+  // it('should render multiple follower items', async () => {
+  //   render(<MockFoloowersList />);
+  //   const followerDivElements = await screen.findAllByTestId(/follower-item/i);
+  //   expect(followerDivElements.length).toBe(5);
+  // });
 });
