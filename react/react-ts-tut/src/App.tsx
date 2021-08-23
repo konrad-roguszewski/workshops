5b56f10c6ce9809f0e06ca43e0484f306c3d00be
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-23 14:47:10 +0200

    Render props

--- react/react-ts-tut/src/App.tsx
+++ react/react-ts-tut/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Counter } from './Counter';
 import { TextField } from './TextField';
 
 // props
@@ -8,9 +9,17 @@ import { TextField } from './TextField';
 const App: React.FC = () => {
   return (
     <div>
-      <TextField text="hello" person={{firstName: "", lastName: ""}}/>
+      <TextField text="hello" person={{ firstName: '', lastName: '' }} />
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ react/react-ts-tut/src/Counter.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+
+interface Props {
+  // children: (
+  //   count: number,
+  //   setCount: React.Dispatch<React.SetStateAction<number>>
+  // ) => JSX.Element | null;
+  children: (data: {
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>;
+  }) => JSX.Element | null;
+}
+
+export const Counter: React.FC<Props> = ({ children }) => {
+  const [count, setCount] = useState(0);
+
+  return <div>{children({ count, setCount })}</div>;
+};
