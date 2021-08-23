1e58746da8d3e47fdeee0061b737c9bf8390513d
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-23 14:28:04 +0200

    Hooks

--- react/react-ts-tut/src/TextField.tsx
+++ react/react-ts-tut/src/TextField.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
 
 interface Person {
   firstName: string;
   lastName: string;
-};
+}
 
 interface Props {
   text: string;
@@ -11,12 +11,21 @@ interface Props {
   i?: number;
   fn?: (bob: string) => string;
   person: Person;
-};
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-export const TextField: React.FC<Props> = ({}) => {
+interface TextNode {
+  text: string;
+}
+
+export const TextField: React.FC<Props> = ({ handleChange }) => {
+  const [count, setCount] = useState<TextNode>({ text: "hello" });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      <input />
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
     </div>
   );
 };
