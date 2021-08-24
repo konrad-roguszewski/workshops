31addc6d41943df7f3c59204663d9e12aa1867ca
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-24 12:37:28 +0200

    Example 1 - array and basic function

--- react/ts-generics-tut/src/tmp.ts
+++ react/ts-generics-tut/src/tmp.ts
@@ -6,3 +6,17 @@
 // props
 // useState
 // jsx generic
+
+export {}
+
+// example 1
+
+type numArray = Array<number>;
+
+const last = <T>(arr: T[]): T => {
+  return arr[arr.length - 1];
+};
+
+const l = last([1, 2, 3]);
+
+const l2 = last<string>(['a', 'b', 'c']);
