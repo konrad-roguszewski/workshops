fc8222b55d5f2449860c91757557612bf2ed2cb6
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-24 13:11:07 +0200

    Example 5 - props

--- react/ts-generics-tut/src/App.tsx
+++ react/ts-generics-tut/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+// example 5 - props
+
+interface Props {
+  name: string;
+}
+
+const HelloWorld: React.FC<Props> = ({ name }) => {
+  return <div>hello {name}</div>;
+};
+
 function App() {
-  return (
-    <div className="App">
-     generics tutorial
-    </div>
-  );
+  return <div className="App">generics tutorial</div>;
 }
 
 export default App;
