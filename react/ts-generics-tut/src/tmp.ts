914a7bec4f31f7ccca3e9670a0cebba70b3eb1af
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-24 13:04:56 +0200

    Example 4 - interfaces

--- react/ts-generics-tut/src/tmp.ts
+++ react/ts-generics-tut/src/tmp.ts
@@ -9,7 +9,7 @@
 
 export {};
 
-// example 1
+// example 1 - array
 
 type numArray = Array<number>;
 
@@ -21,7 +21,7 @@ const l = last([1, 2, 3]);
 
 const l2 = last<string>(['a', 'b', 'c']);
 
-// example 2
+// example 2 - multi generics
 
 const makeArr = <X, Y = number>(x: X, y: Y): [X, Y] => {
   return [x, y];
@@ -31,7 +31,7 @@ const v = makeArr(5, 6);
 const v2 = makeArr('a', 'b');
 const v3 = makeArr<string | null, number>(null, 5);
 
-// example 3
+// example 3 - extends
 
 const makeFullName = <T extends { firstName: string; lastName: string }>(
   obj: T
@@ -44,3 +44,14 @@ const makeFullName = <T extends { firstName: string; lastName: string }>(
 
 const v4 = makeFullName({ firstName: 'bob', lastName: 'junior', age: 15 });
 // const v5 = makeFullName({ another: 'bob', lastName: 'junior', age: 15 });
+
+// example 4 - interfaces
+
+interface Tab<T> {
+  id: string;
+  position: number;
+  data: T;
+};
+
+type NumberTab = Tab<number>;
+type StringTab = Tab<string>;
