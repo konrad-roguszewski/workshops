1b8d4f2e66dc6d692f1cc2d868e6106536bc2eb7
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-24 13:23:27 +0200

    Example 7 - jsx generic

--- react/ts-generics-tut/src/App.tsx
+++ react/ts-generics-tut/src/App.tsx
@@ -8,16 +8,32 @@ interface Props {
 }
 
 const HelloWorld: React.FC<Props> = ({ name }) => {
-
   // example 6 - useState
-  
+
   const [state] = useState<{ fullname: string | null }>({ fullname: '' });
 
   return <div>hello {name}</div>;
 };
 
+// example 7 - jsx generic
+
+interface FormProps<T> {
+  values: T;
+  children: (values: T) => JSX.Element;
+}
+
+const Form = <T extends {}>({ values, children }: FormProps<T>) => {
+  return children(values);
+};
+
 function App() {
-  return <div className="App">generics tutorial</div>;
+  return (
+    <div className="App">
+      <Form<{ firstName: string | null }> values={{ firstName: 'bob' }}>
+        {values => <div>{values.firstName}</div>}
+      </Form>
+    </div>
+  );
 }
 
 export default App;
