9c4a57f166ab712e265f7939c5ece4970747a6e1
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-24 12:24:35 +0200

    Project setup with cra

---
+++ react/ts-generics-tut/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     generics tutorial
+    </div>
+  );
+}
+
+export default App;
---
+++ react/ts-generics-tut/src/tmp.ts
@@ -0,0 +1,8 @@
+// Array
+// last
+// makeArr: 2 generics, return, overwrite inference, default value
+// addFullName: extends
+// interfaces
+// props
+// useState
+// jsx generic
