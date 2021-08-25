182fd218eb3a6173e327bd71abe36129d98ae397
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-25 11:59:02 +0200

    React with ts example

--- react/react-ts-crash-course/src/App.tsx
+++ react/react-ts-crash-course/src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC} from 'react';
 import './App.css';
 
-function App() {
+interface AppProps {
+  sendSearchQuery?(): void;
+};
+
+// type AppProps = {
+//   sendSearchQuery?(): void;
+// };
+
+const App: FC<AppProps> = ({sendSearchQuery = () => undefined}) => {
+  const [searchValue, setSearchValue] = React.useState<string>();
+
+  const onSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const search = () => {
+    sendSearchQuery()
+  };
+
   return (
     <div>
-     hello
+     <input value={searchValue} onChange={onSearchInput} name="search" type="text" />
+     <button onClick={search}>Search</button>
     </div>
   );
 }
