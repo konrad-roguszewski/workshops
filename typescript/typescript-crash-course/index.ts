120bb2b751988f72e98d1eceaa56d00c122e7871
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-27 14:49:23 +0200

    Built-in types

---
+++ typescript/typescript-crash-course/index.ts
@@ -0,0 +1,25 @@
+// Type Number
+let age = 24;
+
+let weight: number = 170;
+
+weight = 180;
+weight = 160;
+weight = "160";
+
+age = 25;
+age = 23;
+age = "25";
+
+// Type String
+let myName = "Laith Harb";
+let myDogName: string = "Doggy";
+
+myName = true;
+myName = "Laith Harb".length;
+myName = "Laith Harb".length === 0;
+
+// Type Boolean
+let isAwesome = false;
+let isAwesome2 = "Laith Harb".length === 0;
+let isBoring : boolean = true;
