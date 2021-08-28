fce46344fea66ac4984b9089a9262095238644bb
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-28 10:02:30 +0200

    Objects

---
+++ typescript/typescript-crash-course/index3.ts
@@ -0,0 +1,31 @@
+// Objects
+
+const obj = {
+  firstName: "Laith",
+  lastName: "Harb",
+  phone: "222-222-222",
+  age: 24,
+  gender: false,
+};
+
+const obj2: {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  age: number;
+  gender: boolean;
+} = {
+  firstName: "",
+  lastName: "",
+  phone: "",
+  age: 23,
+  gender: true,
+};
+
+obj2.age = 34;
+obj2.age = true;
+
+obj2.name
+
+obj2.phone.length
+obj2.age.length
