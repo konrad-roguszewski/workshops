d4256816a94f93c83bbab5c15fc9a4c042b8c0b0
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-30 13:52:20 +0200

    Handling props

---
+++ react/react-with-ts-cc/src/components/List.tsx
@@ -0,0 +1,18 @@
+interface IProps {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
+const List: React.FC<IProps> = ({ people }) => {
+  return (
+    <div>
+      I am a list
+    </div>
+  );
+}
+ 
+export default List;
