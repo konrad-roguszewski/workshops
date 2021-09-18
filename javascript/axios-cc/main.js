f4c9a795b2c1cd59209e9a32f81870dc8a9eff9c
Konrad Roguszewski <aker.hm@gmail.com>
2021-09-18 11:25:10 +0200

    Transform response

--- javascript/axios-cc/main.js
+++ javascript/axios-cc/main.js
@@ -87,7 +87,19 @@ function customHeaders() {
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options = {
+    method: 'post',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    data: {
+      title: 'Hello World'
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data => {
+      data.title = data.title.toUpperCase();
+      return data;
+    })
+  }
+
+  axios(options).then(res => showOutput(res))
 }
 
 // ERROR HANDLING
