fbe5505961920ad644c3f168ac1474c1148351f0
Konrad Roguszewski <aker.hm@gmail.com>
2021-09-18 11:43:33 +0200

    Error handling

--- javascript/axios-cc/main.js
+++ javascript/axios-cc/main.js
@@ -108,7 +108,26 @@ function transformResponse() {
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios
+    .get('https://jsonplaceholder.typicode.com/todoss')
+    .then(res => showOutput(res))
+    .catch(err => {
+      if (err.response) {
+        // Server responded with status other than 200 range
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+
+        if (err.response.status === 404) {
+          alert('Error: Page Not Found');
+        }
+      } else if (err.request) {
+        // Request was made but there is no response
+        console.log(err.request);
+      } else {
+        console.log(err.message);
+      }
+    });
 }
 
 // CANCEL TOKEN
