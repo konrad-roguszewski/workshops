522eca361462b86450ead7a507be5c9477a474f8
Konrad Roguszewski <aker.hm@gmail.com>
2021-09-26 10:13:51 +0200

    Splitting reducers

--- react/redux-fundamentals/3-example-app/src/reducer.js
+++ react/redux-fundamentals/3-example-app/src/reducer.js
@@ -1,76 +1,9 @@
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build sth fun', completed: false, color: 'blue' }
-  ],
-  filters: {
-    status: 'All',
-    colors: []
-  }
-}
-
-function nextTodoId(todos) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
-
 // Use the initial state as a default value
-export default function appReducer(state = initialState, action) {
+export default function appReducer(state, action) {
   // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
     // Do sth here based on the different types of actions
-    case 'todos/todoAdded': {
-      // We need to return a new state object
-      return {
-        // that has all the existing state data
-        ...state,
-        // but has a new array for the `todos` field
-        todos: [
-          // with all of the old todos
-          ...state.todos,
-          // and the new todo object
-          {
-            // use an auto-incrementing numeric Id for this example
-            id: nextTodoId(state.todos),
-            text: action.payload,
-            completed: false
-          }
-        ]
-      }
-    }
-    case 'todos/todoToggled': {
-      return {
-        // Again copy the entire state object
-        ...state,
-        // This time, we need to make a copy of the old todos array
-        todos: state.todos.map(todo => {
-          if (todo.id !== action.payload) {
-            return todo
-          }
-
-          // We've found the todo that has to change. Return a copy:
-          return {
-            ...todo,
-            // Flip the completed flag
-            completed: !todo.completed
-          }
-        })
-      }
-    }
-    case 'filters/statusFilterChanged': {
-      return {
-        // Copy the whole state
-        ...state,
-        // Overwrite the filters value
-        filters: {
-          // copy the other filter fields
-          ...state.filters,
-          // And replace the status field with the new value
-          status: action.payload
-        }
-      }
-    }
+    
     default:
       // If this reducer doesn't recognize the action type, or doesnn't
       // care about this specific action, return the existing state unchanged
