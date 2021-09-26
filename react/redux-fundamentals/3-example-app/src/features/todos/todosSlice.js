1bdc7b147a92d68ddd4567ed37fb207db557332c
Konrad Roguszewski <aker.hm@gmail.com>
2021-09-26 10:36:23 +0200

    Add other actions to reducers

--- react/redux-fundamentals/3-example-app/src/features/todos/todosSlice.js
+++ react/redux-fundamentals/3-example-app/src/features/todos/todosSlice.js
@@ -12,7 +12,6 @@ function nextTodoId(todos) {
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
-      // Can return just the new todos array - no extra object around it
       return [
         ...state,
         {
@@ -33,6 +32,30 @@ export default function todosReducer(state = initialState, action) {
           completed: !todo.completed,
         }
       })
+    }
+    case 'todos/colorSelected': {
+      const { color, todoId } = action.payload
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo
+        }
+
+        return {
+          ...todo,
+          color,
+        }
+      })
+    }
+    case 'todos/todoDeleted': {
+      return state.filter((todo) => todo.id !== action.payload)
+    }
+    case 'todos/allCompleted': {
+      return state.map((todo) => {
+        return { ...todo, completed: true }
+      })
+    }
+    case 'todos/completedCleared': {
+      return state.filter((todo) => !todo.completed)
     }
     default:
       return state
