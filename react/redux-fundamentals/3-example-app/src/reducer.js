470a807531d2c1642f174262762cbd47d3f3df19
Konrad Roguszewski <aker.hm@gmail.com>
2021-09-26 10:52:42 +0200

    Combining reducers

--- react/redux-fundamentals/3-example-app/src/reducer.js
+++ react/redux-fundamentals/3-example-app/src/reducer.js
@@ -1,12 +1,12 @@
-// Use the initial state as a default value
-export default function appReducer(state, action) {
-  // The reducer normally looks at the action type field to decide what happens
-  switch (action.type) {
-    // Do sth here based on the different types of actions
-    
-    default:
-      // If this reducer doesn't recognize the action type, or doesnn't
-      // care about this specific action, return the existing state unchanged
-      return state
+import todosReducer from "./features/todos/todosSlice";
+import filtersReducer from "./features/filters/filtersSlice";
+
+export default function rootReducer(state = {}, action) {
+  // always return a new object for the root state
+  return {
+    // the value od `state.todos` is whatever the todos reducer returns
+    todos: todosReducer(state.todos, action),
+    // For both reducers, we only pass in their slice of the state
+    fliters: filtersReducer(state.filters, action)
   }
 }
