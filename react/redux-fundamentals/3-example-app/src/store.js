1792acfff1eddd592eda5690f656644018893bce
Konrad Roguszewski <aker.hm@gmail.com>
2021-09-27 16:37:25 +0200

    Loading initial state

--- react/redux-fundamentals/3-example-app/src/store.js
+++ react/redux-fundamentals/3-example-app/src/store.js
@@ -1,6 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "./reducer";
 
-const store = createStore(rootReducer)
+let preloadedState
+const persistedTodosString = localStorage.getItem('todos')
+
+if (persistedTodosString) {
+  preloadedState = {
+    todos: JSON.parse(persistedTodosString)
+  }
+}
+
+const store = createStore(rootReducer, preloadedState)
 
 export default store
