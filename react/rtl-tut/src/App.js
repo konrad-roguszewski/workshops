b95fc8a9569de6a5bdbf601a8707c2da099c13f2
Konrad Roguszewski <aker.hm@gmail.com>
2021-10-02 13:37:05 +0200

    RTL: async

--- react/rtl-tut/src/App.js
+++ react/rtl-tut/src/App.js
@@ -1,49 +1,39 @@
 import React from "react";
+import axios from 'axios';
 
-function getUser() {
-  return Promise.resolve({ id: '1', name: 'Robin' });
-}
+const URL = 'http://hn.algolia.com/api/v1/search';
 
 function App() {
-  const [search, setSearch] = React.useState('');
-  const [user, setUser] = React.useState(null);
+  const [stories, setStories] = React.useState([]);
+  const [error, setError] = React.useState(null);
 
-  React.useEffect(() => {
-    const loadUser = async () => {
-      const user = await getUser();
-      setUser(user);
-    };
+  async function handleFetch(event) {
+    let result;
 
-    loadUser();
-  }, []);
-
-  function handleChange(event) {
-    setSearch(event.target.value);
+    try {
+      result = await axios.get(`${URL}?query=React`);
+      
+      setStories(result.data.hits);
+    } catch (error) {
+      setError(error);
+    }
   }
 
   return (
     <div>
-      {user ? <p>Signed in as {user.name}</p> : null}
+      <button type="button" onClick={handleFetch}>
+        Fetch Stories
+      </button>
 
-      <Search value={search} onChange={handleChange}>
-        Search:
-      </Search>
+      {error && <span>Something went wrong ...</span>}
 
-      <p>Searches for {search ? search : '...'}</p>
-    </div>
-  );
-}
-
-export function Search({ value, onChange, children }) {
-  return (
-    <div>
-      <label htmlFor="search">{children}</label>
-      <input
-        id="search"
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
+      <ul>
+        {stories.map((story) => (
+          <li key={story.objectID}>
+            <a href={story.url}>{story.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- react/rtl-tut/src/App.test.js
+++ react/rtl-tut/src/App.test.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import App, { Search } from './App';
+import App from './App';
+
+jest.mock('axios');
 
 describe('App', () => {
-  test('renders App component', async () => {
+  test('fetches stories from an API and displays them', async () => {
+    const stories = [
+      { objectID: '1', title: 'Hello' },
+      { objectID: '2', title: 'React' },
+    ];
+
+    const promise = Promise.resolve({ data: { hits: stories } });
+
+    axios.get.mockImplementationOnce(() => promise);
+
     render(<App />);
 
-    // wait for the user to resolve
-    await screen.findByText(/Signed in as/);
+    await userEvent.click(screen.getByRole('button'));
 
-    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    await act(() => promise);
 
-    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
-
-    expect(
-      screen.getByText(/Searches for JavaScript/)
-    ).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
   });
 
-  describe('Search', () => {
-    test('calls the onChange callback handler', async () => {
-      const onChange = jest.fn();
+  test('fetches stories from an API and fails', async () => {
+    axios.get.mockImplementationOnce(() =>
+      Promise.reject(new Error())
+    );
 
-      render(
-        <Search value="" onChange={onChange}>
-          Search
-        </Search>
-      );
+    render(<App />);
 
-      await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+    await userEvent.click(screen.getByRole('button'));
 
-      expect(onChange).toHaveBeenCalledTimes(10);
-    })
+    const message = await screen.findByText(/Something went wrong/);
+
+    expect(message).toBeInTheDocument();
   });
 });
