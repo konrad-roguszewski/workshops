e37d1792ffd1c34191f8717ceed00506a3bf13ec
Konrad Roguszewski <aker.hm@gmail.com>
2021-10-02 12:08:58 +0200

    RTL: search types

--- react/rtl-tut/src/App.test.js
+++ react/rtl-tut/src/App.test.js
@@ -7,19 +7,6 @@ describe('App', () => {
   test('renders App component', () => {
     render(<App />);
 
-    // implicit assertion
-    // because getByText would throw error
-    // if element wouldn't be there
-    screen.getByText('Search:');
-
-    // explicit assertion
-    // recommended
-    expect(screen.getByText('Search:')).toBeInTheDocument();
-
-    // fails
-    expect(screen.getByText('Search')).toBeInTheDocument();
-
-    // succeeds
-    expect(screen.getByText(/Search/)).toBeInTheDocument();
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 });
