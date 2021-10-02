314ca3070bfc6bf51cd796fbb6df7a2638364eb2
Konrad Roguszewski <aker.hm@gmail.com>
2021-10-02 12:30:24 +0200

    RTL: search variants - findBy

--- react/rtl-tut/src/App.test.js
+++ react/rtl-tut/src/App.test.js
@@ -4,10 +4,17 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  test('renders App component', () => {
+  test('renders App component', async () => {
     render(<App />);
 
-    // So every time you are asserting that an element isn't there, use queryBy. Otherwise default to getBy
-    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+
+    screen.debug();
+
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+
+    screen.debug();
+
+    // For any element that isn't there yet but will be there eventually, use findBy over getBy or queryBy. If you assert for a missing element, use queryBy. Otherwise default to getBy.
   });
 });
