8610b0d96ad980975a0b06fea43e3268aeb7dee2
Konrad Roguszewski <aker.hm@gmail.com>
2021-10-14 16:51:54 +0200

    Working with array and object types

--- react/react-complete-guide/27-react-plus-typescript/basics.ts
+++ react/react-complete-guide/27-react-plus-typescript/basics.ts
@@ -13,3 +13,24 @@ let userName: string;
 userName = 'Max';
 
 let isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Max',
+  age: 32,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
