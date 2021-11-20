bdbc8328fb4a35e2250edc70934a6cab73a34a3a
Konrad Roguszewski <aker.hm@gmail.com>
2021-11-20 08:07:12 +0100

    Computed and constant members

--- typescript/ts-docs-examples/enums.ts
+++ typescript/ts-docs-examples/enums.ts
@@ -29,3 +29,28 @@ enum BooleanLikeHeterogeneousEnum {
   No = 0,
   Yes = "YES",
 }
+
+// Computed and constant members
+enum E {
+  X,
+}
+
+enum E1 {
+  X,
+  Y,
+  Z,
+}
+
+enum E2 {
+  A = 1,
+  B,
+  C,
+}
+
+enum FileAccess {
+  None,
+  Read = 1 << 1,
+  Write = 1 << 2,
+  ReadWrite = Read | Write,
+  G = "123".length,
+}
